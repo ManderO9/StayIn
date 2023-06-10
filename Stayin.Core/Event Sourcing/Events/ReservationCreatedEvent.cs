4eae1854c423c72edc8ffcfb38f837e35a9d30f5
ManderO9 <hossem.chetoui@gmail.com>
2023-06-10 16:20:34 +0100

    typo

---
+++ Stayin.Core/Event Sourcing/Events/ReservationCreatedEvent.cs
@@ -0,0 +1,33 @@
+﻿namespace Stayin.Core;
+
+public class ReservationCreatedEvent : BaseEvent
+{
+    public override async Task Handle(IDataAccess dataAccess)
+    {
+
+        var housePublication = await dataAccess.GetHousePublicationById(appartement!);
+
+        await dataAccess.AddRentalAsync(new Rental()
+        {
+            Id = _id!,
+            HousePublicationId = appartement!,
+            RenterId = user!,
+            StartedDate = checkIn,
+            EndedDate = checkOut,
+            PublicationTitle = housePublication?.Title
+        });
+        await dataAccess.SaveChangesAsync();
+    }
+
+    public string? _id { get; set; }
+    public string? appartement { get; set; }
+    public string? user { get; set; }
+    public string? checkIn { get; set; }
+    public string? checkOut { get; set; }
+    public string? numberOfGuests { get; set; }
+    public string? name { get; set; }
+    public string? phone { get; set; }
+    public string? email { get; set; }
+    public string? price { get; set; }
+    public string? reservedDates { get; set; }
+}
